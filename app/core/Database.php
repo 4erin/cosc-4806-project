b825c9af60cc8586de5d19c8bef2045019e1ae6d
4erin <149449116+4erin@users.noreply.github.com>
2025-07-25 14:42:25 +0000

    starting the project

---
+++ app/core/Database.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Core;
+use PDO;
+use PDOException;
+
+class Database {
+    public static function connect() {
+        $host = "lx0hl.h.filess.io";
+        $port = 3305;
+        $dbname = "cosc4806_wholealoud";
+        $user = "cosc4806_wholealoud";
+        $pass = $_ENV['password'] ?? getenv('password');
+
+        try {
+            return new PDO("mysql:host=$host;port=$port;dbname=$dbname", $user, $pass);
+        } catch (PDOException $e) {
+            die("DB Connection failed: " . $e->getMessage());
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,8 +1,42 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php
+session_start();
 
-</html>
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', '/', $class);
+    require_once __DIR__ . '/app/' . $class . '.php';
+});
+
+use App\Controllers\AController;
+use App\Controllers\MovieHandler;
+
+$action = $_GET['action'] ?? '';
+
+switch ($action) {
+    case 'login':
+        (new AController())->login();
+        break;
+    case 'register':
+        (new AController())->register();
+        break;
+    case 'logout':
+        (new AController())->logout();
+        break;
+    case 'guest':
+        $_SESSION['guest'] = true;
+        header("Location: index.php?action=search");
+        break;
+    case 'search':
+        (new MovieHandler())->search();
+        break;
+    case 'details':
+        (new MovieHandler())->details($_GET['id'] ?? '');
+        break;
+    case 'rate':
+        (new MovieHandler())->rate();
+        break;
+    case 'review':
+        (new MovieHandler())->review($_GET['id'] ?? '');
+        break;
+    default:
+        header("Location: index.php?action=login");
+}
