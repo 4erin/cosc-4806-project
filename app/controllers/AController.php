6f204905adebc34fdd0c2de4bde46ae4c90f44b9
4erin <149449116+4erin@users.noreply.github.com>
2025-07-25 14:45:28 +0000

    created a file called AController for handling the entire login page section

---
+++ app/controllers/AController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controllers;
+
+use App\Models\User;
+
+class AController {
+    public function login() {
+        $error = '';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $user = new User();
+            $id = $user->validate($_POST['username'], $_POST['password']);
+            if ($id) {
+                $_SESSION['user_id'] = $id;
+                unset($_SESSION['guest']);
+                header("Location: index.php?action=search");
+                exit;
+            } else {
+                $error = "Invalid username or password.";
+            }
+        }
+        include "app/views/auth/login.php";
+    }
+
+    public function register() {
+        $error = '';
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $user = new User();
+            $success = $user->create($_POST['username'], $_POST['password']);
+            if ($success) {
+                header("Location: index.php?action=login");
+                exit;
+            } else {
+                $error = "Username already exists.";
+            }
+        }
+        include "app/views/auth/register.php";
+    }
+
+    public function logout() {
+        session_destroy();
+        header("Location: index.php?action=login");
+    }
+}
